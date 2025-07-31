7a355d4c3ce183d78619b7681f0a1f402eae4ca5
Hahn Axel (hahn) <axel.hahn@unibe.ch>
2025-07-31 15:39:11 +0200

    update compiler

--- compile/installer.php
+++ compile/installer.php
@@ -34,7 +34,6 @@ spc-windows-x64.exe                               2025-02-15 17:30:46      3.9M 
 // ----------------------------------------------------------------------
 echo "
   \e[1m$PHP_APP\e[0m
-  DEVELOPMENT ENVIRONMENT
 
   I N S T A L L E R   *   U P D A T E R
 
@@ -134,5 +133,7 @@ if ($bDoBild){
     _skip("Micro already built - php $php_version - extensions \"$PHP_LIBS\"");
 }
 
-_h1("Done. You can run build.php to compile the binary.");  
+_h1("Done. You can run 'build.php' to compile the binary 🎉");
+echo "\n";
+
 // ----------------------------------------------------------------------
